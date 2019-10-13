78aa215a7b447a9a69e91e3f942f67e7dada317f
Teresakao0421 <55570359+Teresakao0421@users.noreply.github.com>
2019-10-13 15:28:20 +0800

    Update week2.py

--- week2.py
+++ week2.py
@@ -2,6 +2,9 @@
 def adjacentElementsProduct(inputArray):
     return max([inputArray[i]*inputArray[i+1] for i in range(len(inputArray)-1)])
     
+#給定整數數組，找到具有最大乘積的相鄰元素對並返回該乘積。
+#range(1,5)代表從1到5(不包含５)>>[1,2,3,4]
+#range(1,5,2)代表從1到5(間隔2)>>[1,3]不包含5
     
     
 5.Below we will define an n-interesting polygon. Your task is to find the area of a polygon for a given n.
@@ -10,7 +13,10 @@ A 1-interesting polygon is just a square with a side of length 1. An n-interesti
 
 def shapeArea(n):
     return n*n+(n-1)*(n-1)
+
+
     
 6.Ratiorg got statues of different sizes as a present from CodeMaster for his birthday, each statue having an non-negative integer size. Since he likes to make things perfect, he wants to arrange them from smallest to largest so that each statue will be bigger than the previous one exactly by 1. He may need some additional statues to be able to accomplish that. Help him figure out the minimum number of additional statues needed.
 
 def makeArrayConsecutive2(statues):
+    
