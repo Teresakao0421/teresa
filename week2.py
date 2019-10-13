30fbaf2b1e28460505cee6cc979d68124bae70b9
Teresakao0421 <55570359+Teresakao0421@users.noreply.github.com>
2019-10-13 15:30:12 +0800

    Update week2.py

--- week2.py
+++ week2.py
@@ -13,10 +13,11 @@ A 1-interesting polygon is just a square with a side of length 1. An n-interesti
 
 def shapeArea(n):
     return n*n+(n-1)*(n-1)
+#找到多邊形的面積
 
 
     
 6.Ratiorg got statues of different sizes as a present from CodeMaster for his birthday, each statue having an non-negative integer size. Since he likes to make things perfect, he wants to arrange them from smallest to largest so that each statue will be bigger than the previous one exactly by 1. He may need some additional statues to be able to accomplish that. Help him figure out the minimum number of additional statues needed.
 
 def makeArrayConsecutive2(statues):
-    
+
