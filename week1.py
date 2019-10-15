e5ce97355928f24d25d7cb4374c58793532abba6
Teresakao0421 <55570359+Teresakao0421@users.noreply.github.com>
2019-10-15 11:51:44 +0800

    Update week1.py

--- week1.py
+++ week1.py
@@ -15,8 +15,8 @@ add(param1, param2)
 
 2.Given a year, return the century it is in. The first century spans from the year 1 up to and including the year 100, the second - from the year 101 up to and including the year 200, etc.
 
-def centuryFromYear(year):
-    return int((year-1) / 100) + 1
+def centuryFromYear(year): #定義
+    return int((year-1) / 100) + 1 #重新定義
 #將年份轉成西元年
     
 
