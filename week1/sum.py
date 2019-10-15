2ce293c93710347f702cce34c28ddd8a60e0d7bb
Teresakao0421 <55570359+Teresakao0421@users.noreply.github.com>
2019-10-15 11:49:55 +0800

    Create sum.py

---
+++ week1/sum.py
@@ -0,0 +1,12 @@
+1.Write a function that returns the sum of two numbers:
+
+def add(param1, param2):
+    return param1+param2
+param1=1
+param2=2
+add(param1, param2)
+
+#基本的加法運算
+# def-定義
+# return-重新定義
+# int{int(number)直接設定成整數，int(string,base)可以任意更換base進位計算十進位的相對值>>>int(10.23)>>>10
